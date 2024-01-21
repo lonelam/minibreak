c81b123ac15a25a12a6325fbb782f185d9e886e0
laizenan <laizenan@gmail.com>
2024-01-22 04:46:54 +0800

    feat: compatible with macos

--- src/wallpaper_fast/macos.rs
+++ src/wallpaper_fast/macos.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::process::Command;
 
 pub async fn set_from_path(path: &str) -> Result<(), Box<dyn Error>> {
     wallpaper::set_from_path(path)?;
@@ -6,5 +7,9 @@ pub async fn set_from_path(path: &str) -> Result<(), Box<dyn Error>> {
 }
 
 pub async fn show_desktop() -> Result<(), Box<dyn Error>> {
+    let script =
+        r#"tell application "System Events" to set visible of every application process to false"#;
+    Command::new("osascript").arg("-e").arg(script).output()?;
+
     Ok(())
 }
--- src/wallpaper_fast/mod.rs
+++ src/wallpaper_fast/mod.rs
@@ -1,6 +1,13 @@
 #[cfg(windows)]
 mod windows;
+#[cfg(windows)]
 pub use self::windows::*;
 
-#[cfg(not(windows))]
+#[cfg(target_os = "linux")]
+mod fallback;
+#[cfg(target_os = "linux")]
 pub use fallback::*;
+
+#[cfg(target_os = "macos")]
+mod macos;
+pub use macos::*;
