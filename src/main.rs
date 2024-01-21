e720cd24110cd7a66a5d3c8ed2c5e2bd4b20f285
laizenan <laizenan@gmail.com>
2024-01-22 04:47:37 +0800

    feat: download 2k by default

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod wallpaper_fast;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wallpaper_url =
-        paper_source::bing::get_latest_bing_wallpaper_url(WallpaperResolution::W3840).await?;
+        paper_source::bing::get_latest_bing_wallpaper_url(WallpaperResolution::W1920).await?;
     println!("The latest BING wallpaper url is: {}", wallpaper_url);
     let wallpaper_path =
         paper_source::download::download_url_to_wallpaper_dir(wallpaper_url).await?;
